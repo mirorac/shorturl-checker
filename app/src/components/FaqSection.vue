c1fd75884cdf90b3ff9427f59ea82b37c30700ec
mirorac <racmiroslav@gmail.com>
2023-01-28 23:03:41 +0100

    improve seo

--- app/src/components/FaqSection.vue
+++ app/src/components/FaqSection.vue
@@ -1,38 +1,37 @@
 <template>
-  <div class="bg-white">
+  <div class="bg-white" itemscope itemtype="https://schema.org/FAQPage">
     <div class="mx-auto max-w-7xl px-6 pt-24 pb-10 lg:px-8">
       <div class="mx-auto max-w-4xl divide-y divide-gray-900/10">
         <h2 class="text-2xl font-bold leading-10 tracking-tight text-gray-900">
           FAQ
         </h2>
         <dl class="mt-10 space-y-6 divide-y divide-gray-900/10">
-          <Disclosure
-            as="div"
+          <div
             v-for="faq in faqs"
             :key="faq.question"
             class="pt-6"
-            v-slot="{ open }"
+            itemscope
+            itemprop="mainEntity"
+            itemtype="https://schema.org/Question"
           >
-            <dt>
-              <DisclosureButton
-                class="flex w-full items-start justify-between text-left text-gray-900"
-              >
-                <span class="text-base font-semibold leading-7">{{
-                  faq.question
-                }}</span>
-                <span class="ml-6 flex h-7 items-center">
-                  <ToggleIcon
-                    class="h-6 w-6"
-                    :class="{ 'rotate-180': open }"
-                    aria-hidden="true"
-                  />
-                </span>
-              </DisclosureButton>
+            <dt
+              class="flex w-full items-start justify-between text-left text-gray-900"
+            >
+              <h2 class="text-base font-semibold leading-7" itemprop="name">
+                {{ faq.question }}
+              </h2>
             </dt>
-            <DisclosurePanel as="dd" class="mt-2 pr-12">
-              <p class="text-base leading-7 text-gray-600">{{ faq.answer }}</p>
-            </DisclosurePanel>
-          </Disclosure>
+            <dd
+              class="mt-2 pr-12"
+              itemscope
+              itemprop="acceptedAnswer"
+              itemtype="https://schema.org/Answer"
+            >
+              <p class="text-base leading-7 text-gray-600" itemprop="text">
+                {{ faq.answer }}
+              </p>
+            </dd>
+          </div>
         </dl>
       </div>
     </div>
@@ -40,14 +39,16 @@
 </template>
 
 <script setup lang="ts">
-import { Disclosure, DisclosureButton, DisclosurePanel } from '@headlessui/vue'
-import { ChevronDown12Filled as ToggleIcon } from '@vicons/fluent'
-
 const faqs = [
   {
-    question: 'What is the purpose of this website?',
+    question: 'Is a link checker safe to use?',
     answer:
-      'This website allows users to uncover the destination of a shortened URL provided by TinyURL, helping people to ensure the safety of links from untrusted sources.',
+      "Yes, this website's link checker is safe to use. It is simply a tool for decoding the destination of a shortened URL and does not collect any personal information or harm your device in any way.",
+  },
+  {
+    question: 'Is your website a short URL checker?',
+    answer:
+      'Yes, our website is a short URL checker that allows users to uncover the destination of a shortened URL provided by TinyURL.',
   },
   {
     question: 'How does this website work?',
@@ -55,14 +56,14 @@ const faqs = [
       'Users can enter a TinyURL into the provided field on the website and the website will show them the destination of the shortened URL.',
   },
   {
-    question: 'Is this website safe to use?',
+    question: 'Does your website also work as a Bitly checker?',
     answer:
-      'Yes, this website is safe to use. It is simply a tool for decoding the destination of a shortened URL and does not collect any personal information or harm your device in any way.',
+      'This website is specifically designed to work with TinyURL. If you wish to preview the destination of a shortened URL from Bitly, you may need to use a different tool.',
   },
   {
-    question: 'Can I use this website for any other shortening service?',
+    question: 'Can I use your website as a URL redirect checker?',
     answer:
-      'This website is specifically designed to work with TinyURL. If you wish to decode the destination of a shortened URL from a different service, you may need to use a different tool.',
+      'Yes, our website can be used as a URL redirect checker. It allows users to enter a shortened URL and preview the destination of the redirect. This can help users ensure the safety of links from untrusted sources.',
   },
   {
     question: 'What should I do if I find a suspicious link?',
